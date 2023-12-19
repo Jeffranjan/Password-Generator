3cbf328f23495d6d5970455aabd092b38081e0d4
Ranjan Gupta <Ranjanguptajeff@gmail.com>
2023-12-19 12:43:06 +0530

    Code Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useCallback, useRef, useEffect } from "react";
 
-<div></div>
 
 function App() {
   const [length, setLength] = useState(8);
@@ -35,7 +34,7 @@ function App() {
     passwordGenerator();
   }, [length, numberAllowed, charAllowed, passwordGenerator]);
   return (
-    <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-purple-500 text-orange-500">
+    <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-purple-500 text-black">
       <h1 className="text-white text-center my-3">Password Generator</h1>
       <div className="flex shadow rounded-lg overflow-hidden mb-4">
         <input
